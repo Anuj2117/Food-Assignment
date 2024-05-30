bc3173c6bdb17592f3d87070230655c7dfb540b7
Anuj2117 <anujkushwaha221@gmail.com>
2024-05-30 23:47:08 +0530

    create new repo for assignment

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>helllo</h1>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
