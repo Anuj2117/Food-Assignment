165d488f4c9baf15e2143ea98437112a86c7e146
Anuj2117 <anujkushwaha221@gmail.com>
2024-06-01 21:31:08 +0530

    add some changes

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -2,7 +2,86 @@ function Hero(){
 
     return(
         <>
-        <h1>hero</h1>
+         <div className="aside">
+            <h1 className="first">"Let us find your" </h1>
+            <h1 className="second">"Forever Food."</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex dolore similique eligendi obcaecati repellendus architecto illum quos voluptatem vero inventore ut fugit dignissimos beatae aliquid dolor, numquam recusandae dolorem!</p>
+
+            <button className="search">Search Now</button>
+            <button className="knowmore">Know More</button>
+         </div>
+         <div className="card">
+           <div className="left"></div>
+           <div className="right">
+            <div className="data">
+                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing numquam voluptate eum, at quo iste velitpa.</h3>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure deleniti dolorem, illo veritatis, error consectetur porro autem ratione nihil dolor ea consequatur corrupti cumque excepturi asperiores voluptatum corporis voluptas necessitatibus.</p>
+                 <button>Get in Touch</button>
+            </div>     
+           </div>
+         </div>
+         <div className="grid">
+            <div className="newcard">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+            <div className="newcard">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+            <div className="newcard">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+            <div className="newcard">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+            <div className="newcard">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+            <div className="newcard">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facilis dolore iste accusamus a laboriosam aspernatur nemo impedit, consequatur deleniti porro. Provident dolore mollitia cum, eum consequuntur sed, quidem eveniet asperiores recusandae dolores unde soluta voluptate consectetur, culpa neque perferendis omnis maiores. Debitis, illo? Voluptates odio veritatis nulla sunt sit.</p>
+                <div className="profile">
+                    <img src="https://c8.alamy.com/comp/2JJAD97/hrithik-roshan-poster-krrish-3-2013-2JJAD97.jpg" alt="" />
+                    <div className="name">
+                        <h4>Gladis Lennon</h4>
+                        <p>Head of SEO</p>
+                    </div>
+                </div>
+            </div>
+         </div>
         </>
     )
 }
